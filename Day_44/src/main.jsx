84103ffb122c21020449ead42d5f372573c3650c
Hoang Thanh <thanhvilang@gmail.com>
2023-11-22 02:52:32 +0700

    update ex44

--- Day_44/src/main.jsx
+++ Day_44/src/main.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Auth0Provider } from "@auth0/auth0-react";
-const domain = import.meta.env.VITE_AUTH0_DOMAIN;
-const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+// const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+// const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
 
-// const domain = "dev-etwyqt7p7eff7e6r.us.auth0.com";
-// const clientId = "PQEDmBvreJxc99xYrqvF2PfGxetSKuCB";
+const domain = "dev-etwyqt7p7eff7e6r.us.auth0.com";
+const clientId = "PQEDmBvreJxc99xYrqvF2PfGxetSKuCB";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
