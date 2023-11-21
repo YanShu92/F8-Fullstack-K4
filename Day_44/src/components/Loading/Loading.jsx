7fe72a092754ae707ef99748d34ab6a79ffa3895
Hoang Thanh <thanhvilang@gmail.com>
2023-11-22 03:02:10 +0700

    update ex44

--- Day_44/src/components/Loading/Loading.jsx
+++ Day_44/src/components/Loading/Loading.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { CubeSpinner } from "react-spinners-kit";
 import "../Loading/loading.scss";
-const Loading = ({ isLoading = false }) => {
+const Loading = () => {
   return (
-    isLoading && (
-      <div className="loadingBox">
-        <div className="overlay-loading"></div>
-        <div className="loading">
-          <CubeSpinner size={70} frontColor="#00ff89" backColor="#686769" />
-        </div>
+    <div className="loadingBox">
+      <div className="overlay-loading"></div>
+      <div className="loading">
+        <CubeSpinner size={70} frontColor="#00ff89" backColor="#686769" />
       </div>
-    )
+    </div>
   );
 };
 
