c27ea1c0ff84803aa80d0f8016023ff79b20fbf6
Hoang Thanh <thanhvilang@gmail.com>
2023-11-22 14:16:41 +0700

    update ex44

--- Day_44/src/main.jsx
+++ Day_44/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { Auth0Provider } from "@auth0/auth0-react";
 const domain = import.meta.env.VITE_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-
+console.log(domain);
 // const domain = "dev-etwyqt7p7eff7e6r.us.auth0.com";
 // const clientId = "PQEDmBvreJxc99xYrqvF2PfGxetSKuCB";
 ReactDOM.createRoot(document.getElementById("root")).render(
