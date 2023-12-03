b6b09e31bca404189bcd8ba2945315d6a704f126
Hoang Thanh <thanhvilang@gmail.com>
2023-12-03 09:25:35 +0700

    ex47

--- Day_47/src/App.jsx
+++ Day_47/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Login from "./Pages/login/Login";
 const App = () => {
   return (
     <div>
--- Day_47/src/Pages/login/Login.jsx
+++ Day_47/src/Pages/login/Login.jsx
@@ -1,43 +1,15 @@
 import React, { useEffect, useState } from "react";
-import "../assets/scss/login.scss";
+import "./login.scss";
 
 const Login = () => {
   const [email, setEmail] = useState("");
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const handleSubmit = async (e) => {
     e.preventDefault();
     const pattern = /((?:[\w\.-]{3,})@(?:[\w\.-]{1,}\.[a-z]{2,}))/gi;
     if (email.match(pattern)) {
     }
   };
-
-  // const checkUser = () => {
-  //   if (document.cookie) {
-  //     const apiKey = getApiKeyCookie();
-  //     if (apiKey) {
-  //       client.setApi(apiKey);
-  //       dispatch({
-  //         type: "login/comeback",
-  //         payload: {
-  //           message: `Chào mừng bạn ${getEmailCookie()} quay trở lại`,
-  //           status: 1,
-  //         },
-  //       });
-  //     }
-  //   } else {
-  //     dispatch({
-  //       type: "logout",
-  //     });
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   checkUser();
-  // }, []);
-
-  // useEffect(() => {
-  //   if (!isLogin) checkUser();
-  // }, [isLogin]);
 
   return (
     <div className="login-page">
